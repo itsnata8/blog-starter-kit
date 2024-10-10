743add207edfd0f8beb7de50bfc3bfd099a03c67
nata <rizqinata8@gmail.com>
2024-10-10 15:24:37 +0700

    feat: admin login process with validation

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controllers;
 
+use App\Models\User;
+use App\Libraries\Hash;
+use App\Libraries\CIAuth;
 use App\Controllers\BaseController;
 use CodeIgniter\HTTP\ResponseInterface;
 
@@ -18,6 +21,62 @@ class AuthController extends BaseController
     }
     public function loginHandler()
     {
-        echo "...login handler process...";
+        $fieldType = filter_var($this->request->getVar('login_id'), FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
+
+        if ($fieldType == 'email') {
+            $isValid = $this->validate([
+                'login_id' => [
+                    'rules' => 'required|valid_email|is_not_unique[users.email]',
+                    'errors' => [
+                        'required' => 'Email is required',
+                        'valid_email' => 'Please, check the email field. It does not appears to be valid.',
+                        'is_not_unique' => 'Email is not exists in our system.'
+                    ]
+                ],
+                'password' => [
+                    'rules' => 'required|min_length[5]|max_length[45]',
+                    'errors' => [
+                        'required' => 'Password is required',
+                        'min_length' => 'Password must have atleast 5 characters in length.',
+                        'max_length' => 'Password must not have characters more than 45 in length.'
+                    ]
+                ]
+            ]);
+        } else {
+            $isValid = $this->validate([
+                'login_id' => [
+                    'rules' => 'required|is_not_unique[users.username]',
+                    'errors' => [
+                        'required' => 'Username is required',
+                        'is_not_unique' => 'Username is not exists in our system.'
+                    ]
+                ],
+                'password' => [
+                    'rules' => 'required|min_length[5]|max_length[45]',
+                    'errors' => [
+                        'required' => 'Password is required',
+                        'min_length' => 'Password must have atleast 5 characters in length.',
+                        'max_length' => 'Password must not have characters more than 45 in length.'
+                    ]
+                ]
+            ]);
+        }
+        if (!$isValid) {
+            return view('backend/pages/auth/login', [
+                'pageTitle' => 'Login',
+                'validation' => $this->validator
+            ]);
+        } else {
+            $user = new User();
+            $userInfo = $user->where($fieldType, $this->request->getVar('login_id'))->first();
+            $check_password = Hash::check($this->request->getVar('password'), $userInfo['password']);
+
+            if (!$check_password) {
+                return redirect()->route('admin.login.form')->with('fail', 'Wrong password')->withInput();
+            } else {
+                CIAuth::setCIAuth($userInfo);
+                return redirect()->route('admin.home');
+            }
+        }
     }
 }
