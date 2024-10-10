a01f59280f61452d08631279b56324ecad2faa87
nata <rizqinata8@gmail.com>
2024-10-10 11:53:34 +0700

    feat: setup login page

---
+++ app/Controllers/AuthController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class AuthController extends BaseController
+{
+    protected $helpers = ['url', 'form'];
+    public function loginForm()
+    {
+        $data = [
+            'pageTitle' => 'Login',
+            'validation' => null
+        ];
+        return view('backend/pages/auth/login', $data);
+    }
+    public function loginHandler()
+    {
+        echo "...login handler process...";
+    }
+}
