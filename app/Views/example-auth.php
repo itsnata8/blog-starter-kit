814e896a7078ecb353cbe15979b7f2721c15eb24
nata <rizqinata8@gmail.com>
2024-10-10 10:42:03 +0700

    feat: intergrated admin template

--- app/Views/example-auth.php
+++ app/Views/example-auth.php
@@ -1,1 +1,6 @@
-example auth....
+<?= $this->extend('backend/layout/auth-layout'); ?>
+<?= $this->section('content'); ?>
+
+--------- Auth Page content here ---------
+
+<?= $this->endSection(); ?>
--- app/Views/example-page.php
+++ app/Views/example-page.php
@@ -1,1 +1,4 @@
-example page....
+<?= $this->extend('backend/layout/pages-layout'); ?>
+<?= $this->section('content'); ?>
+------------- Page Content Here -------------
+<?= $this->endSection(); ?>
